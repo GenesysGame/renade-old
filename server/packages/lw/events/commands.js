12178869761ac4cda84b683f4c6df97222f4722f
BozhkoAlexander <bozhko.inosoft@gmail.com>
2017-06-07 22:27:56 +0300

    added base command support (pos, veh, weapon).

---
+++ server/packages/lw/events/commands.js
@@ -0,0 +1,48 @@
+﻿var weapons = require("./weapons");
+
+var supportedWeapons = {
+    "pistol": weapons.WEAPON_PISTOL,
+    "smg": weapons.WEAPON_SMG,
+    "rifle": weapons.WEAPON_ASSAULTRIFLE,
+    "sniper": weapons.WEAPON_SNIPERRIFLE,
+    "shotgun": weapons.WEAPON_ASSAULTSHOTGUN
+};
+
+module.exports = {
+
+    handle: function (player, command, args) {
+        var handler = registered[command];
+        if (handler != null) {
+            handler(player, args);
+        }
+    }
+
+};
+
+var registered = {
+    "pos": (player, args) => {
+        var pos = player.position;
+        var x = Math.round(pos.x * 1000) / 1000;
+        var y = Math.round(pos.y * 1000) / 1000;
+        var z = Math.round(pos.z * 1000) / 1000;
+        player.outputChatBox("Your position ->  X: " + x + ", Y: " + y + ", Z: " + z);
+    },
+
+    "veh": (player, args) => {
+        var modelName = args[0];
+        if (modelName == null) { return; }
+        var hash = mp.joaat(modelName);
+        var pos = player.position;
+        pos.x = pos.x + 10;
+        pos.z = pos.z + 2;
+        mp.vehicles.new(hash, pos);
+    },
+
+    "weapon": (player, args) => {
+        var modelName = args[0];
+        if (modelName == null) { return; }
+        var hash = supportedWeapons[modelName];
+        if (hash == null) { return; }
+        player.giveWeapon(hash, 1000);
+    }
+};
--- server/packages/lw/events/world.js
+++ server/packages/lw/events/world.js
@@ -1,15 +1,29 @@
-﻿function playerJoinHandler(player) {
+﻿var commands = require("./commands");
+
+function playerJoinHandler(player) {
     console.log(player.name + " joined the server. Total: " + mp.players.length);
 }
 
 function playerQuitHandler(player, exitType, reason) {
     if (exitType != "kicked") {
-        var str = player.name + " left the server (" + exitType + "). Total: " + mp.players.length - 1;
+        var str = player.name + " left the server (" + exitType + "). Total: " + (mp.players.length - 1);
     } else {
-        var str = player.name + " kicked. Reason: " + reason + ". Total: " + mp.players.length - 1;
+        var str = player.name + " kicked. Reason: " + reason + ". Total: " + (mp.players.length - 1);
     }
     console.log(str);
 }
 
-mp.events.add("playerJoin", playerJoinHandler);
-mp.events.add("playerQuit", playerQuitHandler);
+function commandHandler(player, comText) {
+    var arr = comText.split(" ");
+    var cmd = arr[0];
+    arr.shift();
+    commands.handle(player, cmd, arr);
+}
+
+mp.events.add(
+    {
+        "playerJoin": playerJoinHandler,
+        "playerQuit": playerQuitHandler,
+        "playerCommand": commandHandler
+    }
+);
