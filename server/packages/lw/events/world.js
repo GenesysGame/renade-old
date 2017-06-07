6b89f122e8a23e34a94b3978eb2a6b1ddd9431eb
BozhkoAlexander <bozhko.inosoft@gmail.com>
2017-06-07 09:32:23 +0300

    created lw package.

---
+++ server/packages/lw/events/world.js
@@ -0,0 +1,15 @@
+﻿function playerJoinHandler(player) {
+    console.log(player.name + " joined the server. Total: " + mp.players.length);
+}
+
+function playerQuitHandler(player, exitType, reason) {
+    if (exitType != "kicked") {
+        var str = player.name + " left the server (" + exitType + "). Total: " + mp.players.length - 1;
+    } else {
+        var str = player.name + " kicked. Reason: " + reason + ". Total: " + mp.players.length - 1;
+    }
+    console.log(str);
+}
+
+mp.events.add("playerJoin", playerJoinHandler);
+mp.events.add("playerQuit", playerQuitHandler);
