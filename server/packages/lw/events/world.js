051ea7d7d82a5155fb3642a6a3a3f397bed08eaa
BozhkoAlexander <bozhko.inosoft@gmail.com>
2017-06-10 00:19:33 +0300

    implemented simple start player setting (using random).

--- server/packages/lw/events/world.js
+++ server/packages/lw/events/world.js
@@ -2,6 +2,28 @@
 
 function playerJoinHandler(player) {
     console.log(player.name + " joined the server. Total: " + mp.players.length);
+
+    player.model = mp.joaat("MP_M_Freemode_01");
+
+    let hairId = Math.round(Math.random() * 13);
+    var color = Math.round(Math.random() * 5);
+    console.log(player.name + " -> Hair: " + hairId, "Color: " + color);
+    player.setClothes(2, hairId, color, 0);
+
+    let torsoId = Math.round(Math.random() * 50);
+    console.log(player.name + " -> Torso: " + torsoId);
+    player.setClothes(3, 0, 0, 0);
+    player.setClothes(11, torsoId, 0, 0);
+
+    let legsId = Math.round(Math.random() * 77);
+    console.log(player.name + " -> Legs: " + legsId);
+    player.setClothes(4, legsId, 0, 0);
+
+    let feetId = Math.round(Math.random() * 58);
+    console.log(player.name + " -> Feet: " + feetId);
+    player.setClothes(6, feetId, 0, 0);
+
+    player.spawn(new mp.Vector3(-269.2, 6644, 7.4));
 }
 
 function playerQuitHandler(player, exitType, reason) {
@@ -20,10 +42,8 @@ function commandHandler(player, comText) {
     commands.handle(player, cmd, arr);
 }
 
-mp.events.add(
-    {
-        "playerJoin": playerJoinHandler,
-        "playerQuit": playerQuitHandler,
-        "playerCommand": commandHandler
-    }
-);
+mp.events.add({
+    "playerJoin": playerJoinHandler,
+    "playerQuit": playerQuitHandler,
+    "playerCommand": commandHandler
+});
