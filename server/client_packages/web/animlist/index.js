041261a9bb31540d7aaedc10a41f0e5b6d90e376
BozhkoAlexander <bozhko.inosoft@gmail.com>
2017-10-09 23:36:28 +0300

    added real animations (part 1).

--- server/client_packages/web/animlist/index.js
+++ server/client_packages/web/animlist/index.js
@@ -1,30 +1,35 @@
 ﻿// Rize RP. GGame Studio. 1.10.2017
 // Animations list view controller
 
-let animations = [
-    { name: "amb@code_human_cower@male@enter", pr: "enter" },
-    { name: "amb@code_human_cower@male@base", pr: "base" },
-    { name: "amb@code_human_cower@male@exit", pr: "exit" },
-    { name: "amb@code_human_cower@male@exit", pr: "exit_flee" },
-    { name: "amb@code_human_cower@male@idle_a", pr: "idle_a" },
-    { name: "amb@code_human_cower@male@idle_a", pr: "idle_b" },
-    { name: "amb@code_human_cower@male@idle_a", pr: "idle_c" },
-    { name: "amb@code_human_cower@male@idle_b", pr: "idle_d" },
-    { name: "stop", pr: '' }
-];
-
 let list = $('.list');
+var total = 0;
 
 animations.forEach(function (v, i) {
-    list.append($('<option>', {
-        value: i,
-        text: v.name + " " + v.pr
-    }));
+    if (i >= 1000) { return; }
+    var group = $('<optgroup></optgroup>');
+    group.attr('label', v[0]);
+    $.each(v, function (si) {
+        if (si > 0) {
+            var option = $('<option></option>');
+            option.val([v[0], v[si]]);
+            option.text(v[si]);
+            total = total + 1;
+
+            group.append(option);
+        }
+    });
+    list.append(group);
 });
+
+var option = $('<option></option>');
+option.val(['', '']);
+option.text('exit');
+list.append(option);
+
+$('.total').html(total + ' animations');
 
 list.on('change', function (e) {
     let optionSelected = $("option:selected", this);
-    let index = optionSelected.val();
-    let anim = animations[index];
-    mp.trigger('animlist:animationSelected', anim.name, anim.pr);
+    let anim = optionSelected.val().split(',');
+    mp.trigger('animlist:animationSelected', anim[0], anim[1]);
 });
