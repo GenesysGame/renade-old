7a3dcb8f7595ebd6ff0c2b6d4456d090b593d343
Tomas <tom.tomas.36478119@gmail.com>
2017-10-30 14:17:20 +0500

    update loading for animlist
    And repair api

--- server/api/start.js
+++ server/api/start.js
@@ -2,13 +2,32 @@
 var express = require('express');
 var app = express();
 var anim = require('./animations.js');
+var animations = anim.animations;
+var total = 0;
+animations.forEach(function(v, i) {
+	total += 1;
+});
+var pagesize = 200;
+var totalPages  = Math.floor(total / pagesize);
+console.log('Loaded ' + total + ' animations');
 app.get("/admin/animations/get", function(request, response){
     response.header('Content-Type', 'application/json');
     response.header("Access-Control-Allow-Origin", "*");
+    var page = parseInt(request.query.page);
 	var animations = anim.animations;
-    var max = parseInt(request.query.max);
-    var min = parseInt(request.query.min);
-    var list = [];
+    var list = {
+    	page: page,
+    	total: totalPages,
+    	data: []
+    };
+    if(page == 0){
+    	var min = page * pagesize;
+    	var max = min + pagesize;
+    } else{
+    	var min = page * pagesize + 1;
+    	var max = min + pagesize - 1;
+    }
+    console.log(page);
     console.log(min, max);
     animations.forEach(function (v, i) {
     	if(i <= max && i >= min){
@@ -20,13 +39,15 @@ app.get("/admin/animations/get", function(request, response){
 	        		}
 	        	});
 	        }
-		    var pack = { name: v[0] };
-		    pack.props = props;
-		    list.push(pack);
+		    var pack = {
+		    	name: v[0], 
+		    	props: props
+		    };
+		    list.data.push(pack);
     	}
 	});
     response.json(list);
     response.end();
 });
 app.listen(80);
-console.log('Server running on 80');
+console.log('Server running on 80 port');
--- server/client_packages/web/animlist/index.js
+++ server/client_packages/web/animlist/index.js
@@ -1,85 +1,67 @@
-﻿// Rize RP. GGame Studio. 1.10.2017
+﻿// Renade RP. GGame Studio. 1.10.2017
 // Animations list view controller
 
 let list = $('.list');
-var total = 0;
+var totalPages = 0;
 
-function load(e){
+function load(){
     $('.total').text('LOADING...');
-    if(e == 'forward'){
-        $.get( "http://127.0.0.1/admin/animations/get", { min: (last+1), max: (last+200)}, function( data ) {
-            data.forEach(function (v, i) {
-                var group = $('<optgroup></optgroup>');
-                group.attr('label', v['name']);
-                group.attr('id', last+i-199);
-                $.each(v['props'], function (s, p) {
-                    var option = $('<option></option>');
-                    option.val([v['name'], p]);
-                    option.text(p);
-                    group.append(option);
-                });
-                list.append(group);
+    isLoading = true;
+    list.empty();
+    $.get( "http://127.0.0.1/admin/animations/get", { page: page}, function( data ) {
+        page = data.page;
+        totalPages = data.total;
+        data = data.data
+        data.forEach(function (v, i) {
+            var group = $('<optgroup></optgroup>');
+            group.attr('label', v['name']);
+            group.attr('class', page);
+            $.each(v['props'], function (s, p) {
+                var option = $('<option></option>');
+                option.val([v['name'], p]);
+                option.text(p);
+                group.append(option);
             });
-            $('.total').text('');
-        } , "json" );
-        for (var i = last - 801; i >= last-1000; i--) {
-            $('#'+i).remove();
-        }
-        last += 200;
-    } else{
-        if(last > 1000){
-            $.get( "http://127.0.0.1/admin/animations/get", { min: (last-1200), max: (last-1001)}, function( data ) {
-                var groups = [];
-                data.forEach(function (v, i) {
-                    var group = $('<optgroup></optgroup>');
-                    group.attr('label', v['name']);
-                    group.attr('id', last-1000+i);
-                    $.each(v['props'], function (s, p) {
-                        var option = $('<option></option>');
-                        option.val([v['name'], p]);
-                        option.text(p);
-                        group.append(option);
-                    });
-                    groups.push(group);
-                });
-                list.prepend(groups);
-                $('.total').text('');
-            } , "json" );
-            for (var i = last; i > last-200; i--) {
-                $('#'+i).remove();
-            }
-            last -= 200;
-        }
-    }
+            list.append(group);
+        });
+        $('.total').text('/'+totalPages);
+        $('#page').attr('max', totalPages);;
+        $('#page').val(page);
+        isLoading = false;
+    } , "json" );
 }
 
-animations.forEach(function (v, i) {
-    if (i >= 1001 || i <= 0) { return; }
-    var group = $('<optgroup></optgroup>');
-    group.attr('label', v[0]);
-    group.attr('id', i);
-    $.each(v, function (si) {
-        if (si > 0) {
-            var option = $('<option></option>');
-            option.val([v[0], v[si]]);
-            option.text(v[si]);
-            group.append(option);
-        }
-    });
-    list.append(group);
-});
+var page = 0;
+var isLoading = false;
 
-var last = 1000;
+load();
 
-$('.list').scroll(function(){
-    if($('#'+last).offset().top < 300){
-        load('forward');
-    } else{
-        if(last > 1000 && $('#'+(last-999)).offset().top > -300){
-            load('back');
+$('#page').keydown(function(e) {
+    if(e.keyCode === 13 && !isLoading){
+        if($(this).val() > totalPages){
+            $(this).val(totalPages);
+        } else if($(this).val() < 0){
+            $(this).val(0);
         }
+        page = $(this).val();
+        load();
     }
 });
+
+$('#next').click(function(){
+    if(page != totalPages && !isLoading){
+        page += 1;
+        load();
+    }
+});
+
+$('#prev').click(function(){
+    if(page > 0 && !isLoading){
+        page -= 1;
+        load('back');
+    }
+});
+
 
 list.on('change', function (e) {
     let optionSelected = $("option:selected", this);
